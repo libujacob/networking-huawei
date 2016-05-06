fdc589c642507ab48001b87d2d3ceee8f8c7820c
Libu Jacob Varghese <libu.jacob@huawei.com>
2016-05-06 20:26:11 +0530

    Devstack Support

--- devstack/plugin.sh
+++ devstack/plugin.sh
@@ -2,7 +2,13 @@
 
 DIR_HUAWEI=$DEST/networking-huawei
 
-if is_service_enabled net-huawei; then
+function install_networking_huawei {
+    cd $DIR_HUAWEI
+    sudo python setup.py install
+    sudo pip install -r requirements.txt
+}
+
+if is_service_enabled huawei-ac; then
 
     if [[ "$1" == "source" ]]; then
         # no-op
@@ -10,9 +16,10 @@ if is_service_enabled net-huawei; then
     fi
 
     if [[ "$1" == "stack" && "$2" == "pre-install" ]]; then
-        echo "Pre-install Huawei AC"
+        echo "Configuring Huawei AC"
     elif [[ "$1" == "stack" && "$2" == "install" ]]; then
         echo "Installing Huawei AC"
+        install_networking_huawei
     elif [[ "$1" == "stack" && "$2" == "post-config" ]]; then
         echo "Post config Huawei AC"
     elif [[ "$1" == "stack" && "$2" == "extra" ]]; then
@@ -21,6 +28,9 @@ if is_service_enabled net-huawei; then
 
     if [[ "$1" == "unstack" ]]; then
         echo "Stop Huawei AC"
+        cd $DIR_HUAWEI
+        sudo pip uninstall -q -y networking-huawei
+        sudo rm -r build networking_huawei.egg-info
     fi
 
     if [[ "$1" == "clean" ]]; then
