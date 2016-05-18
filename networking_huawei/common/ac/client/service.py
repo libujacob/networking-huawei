aaf556ad6d536a22a581554479e58f844b4760df
Libu Jacob Varghese <libuparayil@gmail.com>
2016-05-18 23:33:43 +0530

    Review commits

--- networking_huawei/common/ac/client/service.py
+++ networking_huawei/common/ac/client/service.py
@@ -1,6 +1,5 @@
 # coding:utf-8
-# Copyright (c) 2016 OpenStack Foundation.
-# All Rights Reserved.
+# Copyright 2016 Huawei Technologies Co. Ltd. All rights reserved.
 #
 #    Licensed under the Apache License, Version 2.0 (the "License"); you may
 #    not use this file except in compliance with the License. You may obtain
